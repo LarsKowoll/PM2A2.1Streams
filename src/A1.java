51cccfcacb300239bc3571424fe4e7416c983860
XXXDragonSlayer <you@example.com>
2018-11-04 17:50:00 +0100

    The clock ticks life away

--- src/A1.java
+++ src/A1.java
@@ -7,16 +7,29 @@ import java.util.stream.Stream;
 
 public class A1<T> {
 	public static void main(String[] args) {
-		String eingaben[] = { "Eingabe ", "Äußeres ", null, "Strassen-Feger", " ein Haus" };
+		String eingaben[] = { "Eingabe ", "Äußeres ", null, "Strassen-Feger", " ein Haus" };	
+		veraendern(eingaben);
+	}
+	
+	public static List<String> veraendern(String[] eingaben) {
 		Stream<String> stringStream = Stream.of(eingaben);
 		List<String> myList = stringStream
 				.filter(x -> x!=null)
 				.map(String::trim)
 				.map(x -> x.toUpperCase())
-				
+				.map(x -> x.replace("Ä","AE").replace("Ö","OE").replace("Ü","UE").replace("ß","SS") )
+                .map(x -> x = kuerzenAufAcht(x))
 				.collect(Collectors.toList());
 
 		myList.forEach(System.out::println);
-	}
+		return myList;
 		
+	}
+	
+	private static String kuerzenAufAcht(String str){
+        if(str.length() > 8){
+            return str = str.substring(0,8);
+        } 
+        return str;
+    }		
 }
---
+++ src/A1Test.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class A1Test {
+	String[] eingaben = { "Eingabe ", "Äußeres ", null, "Strassen-Feger", " ein Haus" };;
+	
+	public A1Test(){
+		
+	}
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testVeraendern() {
+		veraendern(eingaben);
+	}
+
+}
