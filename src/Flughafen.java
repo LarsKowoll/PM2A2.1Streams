d0ea79f557347ebd23c945bd11877db6cfeaa753
philthefox <spamus@posteo.de>
2018-11-06 22:15:39 +0100

    gelandete Flugzeuge wollen nicht mehr starten :(

--- src/Flughafen.java
+++ src/Flughafen.java
@@ -7,49 +7,64 @@ public class Flughafen implements Runnable {
 	private int _anzahlFlugzeuge;
 	private List<Flugzeug> _flugzeuge;
 	private HashMap<Flugzeug, Thread> _threadMap;
+	private long _zeitInit;
+	private boolean _flugzeugGelandet;
 	
-	public Flughafen() {
+	public Flughafen(int anzahlFlugzeuge) {
+		_anzahlFlugzeuge = anzahlFlugzeuge;
+		_zeitInit = System.currentTimeMillis();
 		_flugzeuge = new ArrayList<Flugzeug>();
 		_threadMap = new HashMap<Flugzeug, Thread>();
+		_flugzeugGelandet = false;
 	}
 	
 	public void landen(Flugzeug flugzeug) {
-		// Thread beenden
-		Thread flugzeugThread = _threadMap.get(flugzeug);
-		flugzeugThread.interrupt();
-		
-		flugzeug.istGelandet();
-		_threadMap.remove(flugzeug);
-		
-		// Konsolenausgabe
-		System.out.println(flugzeug.toString() + " ist gelandet;  Zeit: " + LocalTime.now().toString());
+		try {
+			// Thread beenden
+			Thread flugzeugThread = _threadMap.get(flugzeug);
+			if (!flugzeugThread.isInterrupted()) {
+				
+				flugzeugThread.interrupt();
+				
+				_threadMap.remove(flugzeug);
+				
+				// Konsolenausgabe
+				System.out.println(flugzeug.toString() + " ist gelandet"); // + ";  Zeit: " + LocalTime.now().toString());
+				
+				_flugzeugGelandet = true;
+				
+				flugzeug.setStatus(Status.GELANDET);
+			}
+		} catch (NullPointerException e) {
+			
+		}
 	}
 
 	@Override
-	public void run() {
-		List<Flugzeug> zuEntfernendeFlugzeuge = new ArrayList<Flugzeug>();
-		boolean startNeuesFlugzeug = false;
-		erzeugeFlugzeug();
-		erzeugeFlugzeug();
-		erzeugeFlugzeug();
+	public void run() {		
+		// Flugzeuge erstellen
+		for (int i = 0; i < _anzahlFlugzeuge; i++) {
+			erzeugeFlugzeug();
+		}
+		// Flugzeuge starten
+		for (Flugzeug flugzeug: _flugzeuge) {
+			starten(flugzeug);
+		}
 		
 		while(true) {
 			for (Flugzeug flugzeug: _flugzeuge) {
-				if (flugzeug.istImLandeanflug()) {
-					landen(flugzeug);
-					zuEntfernendeFlugzeuge.add(flugzeug);
-					startNeuesFlugzeug = true;
+				flugzeug.setZeit(System.currentTimeMillis() - _zeitInit);
+			}
+			
+			if (_flugzeugGelandet) {
+				// neues Flugzeug starten
+				for (Flugzeug flugzeug: _flugzeuge) {
+					if (flugzeug.getStatus() == Status.GELANDET) {
+						starten(flugzeug);
+						_flugzeugGelandet = false;
+						break;
+					}
 				}
-			}
-			
-			for (Flugzeug flugzeug: zuEntfernendeFlugzeuge) {
-				_flugzeuge.remove(flugzeug);
-			}
-			zuEntfernendeFlugzeuge.clear();
-			
-			if (startNeuesFlugzeug) {
-				erzeugeFlugzeug();
-				startNeuesFlugzeug = false;
 			}
 			
 			try {
@@ -60,26 +75,31 @@ public class Flughafen implements Runnable {
 		}
 	}
 	
-	private void erzeugeFlugzeug() {
-		// Flugzeug erstellen
-		String id = "Flugzeug " + _anzahlFlugzeuge;
-		Flugzeug flugzeug = new Flugzeug(id, 0, this, 0);
-		
+	private void starten(Flugzeug flugzeug)
+	{
 		// Thread erstellen und starten
+		flugzeug.setStartzeit(System.currentTimeMillis() - _zeitInit);
+		flugzeug.setStatus(Status.IM_FLUG);
 		Thread flugzeugThread = new Thread(flugzeug);
 		flugzeugThread.start();
 
 		_threadMap.put(flugzeug, flugzeugThread);
 		
-		_flugzeuge.add(flugzeug);
-		_anzahlFlugzeuge++;
-		
 		// Konsolenausgabe
-		System.out.println(flugzeug.toString() + " ist gestartet; Zeit: " + LocalTime.now().toString());
+		System.out.println(flugzeug.toString() + " ist gestartet"); // + "; Zeit: " + LocalTime.now().toString());
+	}
+	
+	private void erzeugeFlugzeug() {
+		// Flugzeug erstellen
+		int number = _flugzeuge.size() + 1;
+		String id = "Flugzeug " + number;
+		Flugzeug flugzeug = new Flugzeug(id, this);
+		_flugzeuge.add(flugzeug);
 	}
 	
 	public static void main(String[] args) {
-		Thread flughafenThread = new Thread(new Flughafen());
+		int anzahlFlugzeuge = 10;
+		Thread flughafenThread = new Thread(new Flughafen(anzahlFlugzeuge));
 		flughafenThread.start();
 	}
 }
--- src/Flugzeug.java
+++ src/Flugzeug.java
@@ -2,17 +2,17 @@
 public class Flugzeug implements Runnable {
 	private Flughafen _flughafen;
 	private String _id;
-	private int _flugdauer;
-	private int _startzeit;
+	private long _flugdauer;
+	private long _startzeit;
 	private Status _status;
-	public int _zeit;
+	private long _zeit;
 	
-	public Flugzeug(String id, int flugdauer, Flughafen flughafen, int startzeit) {
+	public Flugzeug(String id, Flughafen flughafen) {
 		_flughafen = flughafen;
 		_id = id;
-		_flugdauer = flugdauer;
-		_startzeit = startzeit;
-		_status = Status.IM_FLUG;
+		_flugdauer = 0;
+		_startzeit = 0;
+		_status = Status.GELANDET;
 	}
 	
 	@Override
@@ -22,35 +22,47 @@ public class Flugzeug implements Runnable {
 			while ((random < 3) || (random > 8)) {
 				random = (int) (Math.random() * 10);
 			}
+			
+			_flugdauer = (_zeit - _startzeit) / 1000;
+			
 			if (_flugdauer == random) {
 				_status = Status.IM_LANDEANFLUG;
 			}
+			
+			checkStatus();
 			
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				// System.out.println("Thread Exception");
-			}
-			
-			_flugdauer++;
+			}			
 		}
 	}
 	
-	public boolean istImLandeanflug()
-	{
-		return _status == Status.IM_LANDEANFLUG;
+	public Status getStatus() {
+		return _status;
 	}
 	
-	public void istGelandet() {
-		_status = Status.GELANDET;
+	public void setStatus(Status status) {
+		_status = status;
+	}
+	
+	private void checkStatus() {
+		if (_status == Status.IM_LANDEANFLUG) {
+			_flughafen.landen(this);
+		}
 	}
 	
 	public String toString() {
-		return _id + " (Flugdauer: " + _flugdauer + "s)";
+		return _id + " (Startzeit: " + _startzeit / 1000 + "; Flugdauer: " + _flugdauer + "s)";
 	}
 	
-	public void setZeit(int neueZeit) {
-		_zeit = neueZeit;
+	public void setZeit(long zeit) {
+		_zeit = zeit;
+	}
+	
+	public void setStartzeit(long startzeit) {
+		_startzeit = startzeit;
 	}
 
 }
